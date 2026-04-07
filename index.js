4342fa1c14bea5e3f014637d6052443a25f24425
rayhan <rayhanzz772@gmail.com>
2026-04-07 15:20:51 +0700

    feat: Update allowedOrigins to include additional localhost for CORS configuration

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const app = express()
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173', // common vite port
+  'http://localhost:5174',
   'http://127.0.0.1:3000',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
 ]
 
 app.use(
