c05a14031918949331ae4dc887e112cb2f2d5fba
rayhan <rayhanzz772@gmail.com>
2026-04-08 23:03:33 +0700

    feat: add production origins to CORS allowed list

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
+
+    // ✅ production
+  'https://crypta.rayhanprojects.site',
+  'https://www.crypta.rayhanprojects.site'
 ]
 
 app.use(
